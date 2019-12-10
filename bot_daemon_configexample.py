962d7595fa792b9376f41299f83831b9bf9ff83f
sergio <sergioatamorim@gmail.com>
2019-12-10 20:19:19 -0300

    config file example for bot_daemon

---
+++ bot_daemon_configexample.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+# The config file needs to be named 'bot_daemon_config.py
+# and be placed on the same directory as the file bot_daemon.py'
+
+bot_token = 'bOt-T0keN_ExaMplE'

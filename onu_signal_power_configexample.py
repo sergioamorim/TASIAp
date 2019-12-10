5633dd0bfbda51239fc67dca9ad50969e9cf44a2
sergio <sergioatamorim@gmail.com>
2019-12-10 20:51:46 -0300

    config file example for onu_signal_power

---
+++ onu_signal_power_configexample.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+# The config file needs to be named 'onu_signal_power_config.py'
+# and be placed on the same directory as the file onu_signal_power.py
+
+telnet = {'ip': '255.255.255.255',
+          'port': 65535,
+          'user': 'user_example',
+          'password': 'password_example',
+          'password_sudo': 'password_sudo_example'}

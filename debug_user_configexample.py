26d6836d6e660bb04f3e4daedd746a6072984bf6
sergio <sergioatamorim@gmail.com>
2019-12-10 20:34:52 -0300

    config file example for debug_user

---
+++ debug_user_configexample.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# The config file needs to be named 'debug_user_config.py'
+# and be placed on the same directory as the file debug_user.py
+
+mysql = {'host': '255.255.255.255',
+  		 'username': 'username_example',
+  	     'password': 'password_example',
+  		 'database': 'database_example',
+  		 'radius_acct_table': 'radius_acct_table_example',
+  		 'login_table': 'login_table_example',
+  		 'clientes_table': 'clientes_table_example'}
+
+telnet = {'ip': '255.255.255.255',
+      	  'port': 65535,
+      	  'user': 'user_example',
+      	  'password': 'password_example',
+      	  'password_sudo': 'password_sudo_example'}

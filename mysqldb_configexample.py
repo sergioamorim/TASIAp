80fd4f08a0eeddb40b545921d919315e5c4f91c4
sergio <sergioatamorim@gmail.com>
2019-12-13 17:55:35 -0300

    fixed variables

--- mysqldb_configexample.py
+++ mysqldb_configexample.py
@@ -3,10 +3,10 @@
 # The config file needs to be named 'mysqldb_config.py'
 # and be placed on the same directory as the files that imports it
 
-host = '255.255.255.255',
-username = 'someusername',
-password = 'somepassword',
-database = 'somedatabase',
-radius_acct_table = 'radius_acct',
-login_table = 'login',
+host = '255.255.255.255'
+username = 'someusername'
+password = 'somepassword'
+database = 'somedatabase'
+radius_acct_table = 'radius_acct'
+login_table = 'login'
 clientes_table = 'clientes'

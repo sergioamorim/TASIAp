d08b4053cf44294ad28d48ef2dcb5fdbb0f1f4f6
sergio <sergioatamorim@gmail.com>
2019-12-13 16:35:11 -0300

    added is_cto_id to return name of cto if any

--- user_from_onu.py
+++ user_from_onu.py
@@ -4,7 +4,10 @@
 import argparse
 import logging
 from telnetlib import Telnet
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 import telnet_config
+import mysqldb_config
 
 logger = logging.getLogger('user_from_onu')
 logger.setLevel(logging.INFO)
@@ -45,7 +48,28 @@ def get_next_value(tn, char):
   logger.debug('get_next_value: return: {0}'.format(value[:-1]))
   return value[:-1].decode('utf-8')
 
-def get_username_by_onu_id(tn, onu_id):
+def sanitize_cto_name(cto_name):
+  ## on development
+
+def is_cto_id(session, onu_id):
+  sql_query = session.execute("SELECT DISTINCT CalledStationID FROM {0} WHERE CalledStationID LIKE :onu_id ORDER BY AcctStartTime DESC LIMIT 1;".format(
+    mysqldb_config.radius_acct_table), {'onu_id': onu_id}).first()
+  if sql_query:
+    return sanitize_cto_name(login_query[0])
+  if onu_id[:1] == '1':
+    board = '12'
+  else:
+  # elif onu_id[:1] == '2':
+    board = '14'
+  pon = onu_id[1:2]
+  onu_number = onu_id[2:] if onu_id[2:3] != '0' else onu_id[3:]
+  sql_query = session.execute("SELECT DISTINCT CalledStationID FROM {0} WHERE CalledStationID LIKE :cto_like_name ORDER BY AcctStartTime DESC LIMIT 1;".format(
+    mysqldb_config.radius_acct_table), {'cto_like_name': 'P{0}-PON{1}-ONU{2}'.format(board, pon, onu_number)}).first()
+  if sql_query:
+    return sanitize_cto_name(login_query[0])
+  return None
+
+def get_username_by_onu_id(session, tn, onu_id):
   ## on development
 
 def main():
@@ -56,10 +80,14 @@ def main():
   onu_id = None
   if args.i:
     onu_id = str(args.i)
+
+  engine = create_engine('mysql://{0}:{1}@{2}/{3}'.format(mysqldb_config.username, mysqldb_config.password, mysqldb_config.host, mysqldb_config.database), encoding='latin1')
+  Session = sessionmaker(bind=engine)
+  session = Session()
   
   with Telnet(telnet_config.ip, telnet_config.port) as tn:
     connect_su(tn)
-    username = get_username_by_onu_id(tn, onu_id)
+    username = get_username_by_onu_id(session, tn, onu_id)
     print(username)
 
 

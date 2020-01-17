d30c93454b0ad3233d14582d1b6d9abec5d436d7
sergio <sergioatamorim@gmail.com>
2020-01-17 10:55:05 -0300

    renamed files removed

--- authorize_onu_configexample.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-# The config file needs to be named 'authorize_onu_config.py'
-# and be placed on the same directory as the file authorize_onu.py
-
-snmp_community = 'community_example'
--- bot_daemon_configexample.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-# The config file needs to be named 'bot_daemon_config.py'
-# and be placed on the same directory as the file bot_daemon.py
-
-bot_token = 'bOt-T0keN_ExaMplE'
-authorized_users = [01234567, 89012345]

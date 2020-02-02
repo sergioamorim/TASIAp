0e2723d548f6995043c78987d1373533a5f80052
sergio <sergioatamorim@gmail.com>
2020-02-02 18:13:34 -0300

    set_cvlan snmp_config now independs on authorize_onu

--- snmp_configexample.py
+++ snmp_configexample.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 # The config file needs to be named 'snmp_config.py'
-# and be placed on the same directory as the file authorize_onu.py
+# and be placed on the same directory as the file onu_set_cvlan.py
 
+ip = '255.255.255.255'
 community = 'community_example'

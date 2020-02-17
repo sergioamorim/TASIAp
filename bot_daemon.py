efa465512442932da3f797ff156b5c17b4058a18
sergio <sergioatamorim@gmail.com>
2020-02-17 07:48:12 -0300

    string related functions are now string_common

--- bot_daemon.py
+++ bot_daemon.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3.8
+# coding=utf-8
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackQueryHandler, Filters
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton, ParseMode
@@ -15,6 +15,7 @@ from mysql_common import get_mysql_session, user_exists
 from telnetlib import Telnet
 import telnet_config
 from telnet_common import connect_su
+from string_common import is_onu_id_valid, is_vlan_id_valid, is_int, get_onu_info_string, get_onu_id_from_repr
 
 logger = logging.getLogger('bot_daemon')
 logger.setLevel(logging.DEBUG)
@@ -28,12 +29,6 @@ logger.addHandler(stream_handler)
 
 def is_user_authorized(user_id):
   return True if user_id in bot_config.permissions[inspect.stack()[1].function] else False
-
-def is_onu_id_valid(onu_id):
-  return is_int(onu_id) and int(onu_id) > 1100 and int(onu_id) < 3900 and int(onu_id[2:]) > 0 and int(onu_id[1:2]) > 0 and int(onu_id[1:2]) < 9
-
-def is_vlan_id_valid(vlan_id):
-  return is_int(vlan_id) and int(vlan_id) > 0 and int(vlan_id) < 4096
 
 def create_link_changing_command_list(link):
   return ['ssh', '-p', '{0}'.format(bot_config.mk_link['ssh']['port']), '{0}@{1}'.format(bot_config.mk_link['ssh']['user'], bot_config.mk_link['ssh']['ip']), '/system',  'script', 'run', '{0}'.format(bot_config.mk_link['script'][link])]
@@ -62,35 +57,6 @@ def get_signal(onu_id):
   elif signal == 'error':
     return 'erro não especificado.'
   return signal
-
-def is_int(s):
-  try:
-    int(s)
-    return True
-  except ValueError:
-    return False
-
-def get_onu_info_string(onu_repr=None, onu_id=None, cvlan=None, serial=None):
-  signal = None
-  if onu_repr:
-    onu_repr_pattern = "([0-9A-Z]{4}[0-9A-Fa-f]{8})',pon='<Pon\(pon_id='[0-9]',board='<Board\(board_id='[0-9]{2}'\)>',last_authorized_onu_number='[0-9]+'\)>',onu_type='.*',number='[0-9]+',cvlan='(N?o?n?e?[0-9]{0,4})"
-    regex_result = re.findall(onu_repr_pattern, onu_repr)
-    serial = regex_result[0][0]
-    if (cvlan := regex_result[0][1]) != 'None':
-      cvlan = cvlan[0]
-    onu_id = get_onu_id_from_repr(onu_repr)
-  else:
-    signal = get_signal(onu_id)
-  return 'ID: {0}{1}\nSerial: {2}{3}'.format(onu_id, '\nCVLAN: {0}'.format(cvlan) if cvlan else '', serial, '\nSinal: {0}'.format(signal) if signal else '')
-
-def get_onu_id_from_repr(onu_repr):
-  onu_repr_pattern = "([0-9])',board='<Board\(board_id='([0-9]{2})'\)>',last_authorized_onu_number='[0-9]+'\)>',onu_type='.*',number='([0-9]+)"
-  regex_result = re.findall(onu_repr_pattern, onu_repr)
-  board = regex_result[0][1]
-  pon = regex_result[0][0]
-  onu_number = regex_result[0][2]
-  onu_id = '{0}{1}{2}{3}'.format('1' if board == '12' else '2', pon, '0' if int(onu_number) < 10 else '', onu_number)
-  return onu_id
 
 def start(update, context):
   logger.debug('start handler: message from {0}{1}{2}({3}) received: {4}'.format(update.message.from_user.first_name, ' {0}'.format(update.message.from_user.last_name) if update.message.from_user.last_name else '', ' - @{0} '.format(update.message.from_user.username) if update.message.from_user.username else '', update.message.from_user.id, update.message.text))
---
+++ string_common.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3.8
+# coding=utf-8
+
+import re
+
+def is_onu_id_valid(onu_id):
+  return is_int(onu_id) and int(onu_id) > 1100 and int(onu_id) < 3900 and int(onu_id[2:]) > 0 and int(onu_id[1:2]) > 0 and int(onu_id[1:2]) < 9
+
+def is_vlan_id_valid(vlan_id):
+  return is_int(vlan_id) and int(vlan_id) > 0 and int(vlan_id) < 4096
+
+def is_serial_valid(serial):
+  return if re.findall('([0-9A-Z]{4}[0-9A-Fa-f]{8})', serial)
+
+def is_int(s):
+  try:
+    int(s)
+    return True
+  except ValueError:
+    return False
+
+def get_onu_info_string(onu_repr=None, onu_id=None, cvlan=None, serial=None):
+  signal = None
+  if onu_repr:
+    onu_repr_pattern = "([0-9A-Z]{4}[0-9A-Fa-f]{8})',pon='<Pon\(pon_id='[0-9]',board='<Board\(board_id='[0-9]{2}'\)>',last_authorized_onu_number='[0-9]+'\)>',onu_type='.*',number='[0-9]+',cvlan='(N?o?n?e?[0-9]{0,4})"
+    regex_result = re.findall(onu_repr_pattern, onu_repr)
+    serial = regex_result[0][0]
+    if (cvlan := regex_result[0][1]) != 'None':
+      cvlan = cvlan[0]
+    onu_id = get_onu_id_from_repr(onu_repr)
+  else:
+    signal = get_signal(onu_id)
+  return 'ID: {0}{1}\nSerial: {2}{3}'.format(onu_id, '\nCVLAN: {0}'.format(cvlan) if cvlan else '', serial, '\nSinal: {0}'.format(signal) if signal else '')
+
+def get_onu_id_from_repr(onu_repr):
+  onu_repr_pattern = "([0-9])',board='<Board\(board_id='([0-9]{2})'\)>',last_authorized_onu_number='[0-9]+'\)>',onu_type='.*',number='([0-9]+)"
+  regex_result = re.findall(onu_repr_pattern, onu_repr)
+  board = regex_result[0][1]
+  pon = regex_result[0][0]
+  onu_number = regex_result[0][2]
+  onu_id = '{0}{1}{2}{3}'.format('1' if board == '12' else '2', pon, '0' if int(onu_number) < 10 else '', onu_number)
+  return onu_id

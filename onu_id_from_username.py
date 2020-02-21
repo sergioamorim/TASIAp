d3b4d84702f6dc0b61428f92ec711393c49ac4e3
sergio <sergioatamorim@gmail.com>
2020-02-21 14:12:41 -0300

    improvements onu_id_from_username diagnosing

--- onu_id_from_username.py
+++ onu_id_from_username.py
@@ -10,6 +10,7 @@ import mysqldb_config
 import telnet_config
 from telnet_common import connect_su, str_to_telnet, get_next_value
 from string_common import sanitize_cto_vlan_name
+from mysql_common import get_mysql_session, reauthorize_user
 
 logger = logging.getLogger('onu_id_from_username')
 logger.setLevel(logging.INFO)
@@ -70,24 +71,54 @@ def format_pon_name(vlan_name):
     return 'slot {0} link {1}'.format(vlan_name[7:9], vlan_name[13:14])
   return None
 
+def diagnose_onu_not_found(pon, query_result, cto_name):
+  if query_result['CalledStationId'][3:5] != '00':
+    return '\nUsuário está desconectado. Última conexão através da ONU da {0}.\nPossíveis problemas:\n- roteador desligado, travado ou desconectado da ONU;\n- ONU travada, sem sinal ou desligada. Verifique o sinal da ONU com o comando "/sinal {1}".'.format(cto_name, query_result['CalledStationId'][1:5])
+  elif pon:
+    board_number = '12' if query_result['CalledStationId'][1:2] == '1' else '14'
+    return '\nUsuário está desconectado. Última conexão através de FIBRA na Placa {0} PON {1}.\nPossíveis problemas:\n- roteador desligado ou desconectado da ONU;\n- ONU travada, sem sinal ou desligada.'.format(board_number, query_result['CalledStationId'][2:3])
+  else:
+    return '\nUsuário não conecta por ONU - vlan: {0}'.format(query_result['CalledStationId'][1:5])
+
+def diagnose_login(session, query_result, username):
+  if query_result['sucess'] == 0:
+    if (login_result := session.execute('SELECT cliente_id, pass, enable FROM login WHERE user = :username;', {'username': username}).first()):
+      if login_result['pass'] == query_result['pass']:
+        if login_result['enable'] == 1:
+          if client_status := session.execute('SELECT status FROM clientes WHERE id = :clientid;', {'clientid': login_result['cliente_id']}).scalar():
+            if client_status == 1:
+              reauthorize_user(session, username)
+              return '\nUsuário não conectado. Roteador encontrado, usuário e senha corretos - reinicie o roteador.'
+            elif client_status == 2:
+              return '\nUsuário não conectado pois cliente está bloqueado por pendência financeira. Usuário e senha corretos.'
+            elif client_status == 0:
+              return '\nUsuário não conectado pois cadastro do cliente está desativado. Usuário e senha corretos.'
+            return '\nUsuário não conectado pois cliente está bloquado manualmente. Usuário e senha corretos.'
+          return '\nErro. Cadastro do cliente não encontrado.'
+        return '\nUsuário não conectado pois o login está desativado. Roteador encontrado, usuário e senha corretos.'
+      return '\nUsuário não conectado. Roteador encontrado, senha incorreta.\nUsuário: {0}\nSenha correta: {1}\nSenha informada: {2}'.format(username, login_result['pass'], query_result['pass'])
+    return '\nErro. Login não encontrado. Verifique o usuário e senha no roteador.'
+  return '\nErro. Conexão do usuário não encontrada. Reinicie o roteador.'
+
 def find_onu_by_user(username):
-  engine = create_engine('mysql://{0}:{1}@{2}/{3}'.format(mysqldb_config.username, mysqldb_config.password, mysqldb_config.host, mysqldb_config.database), encoding='latin1')
-  Session = sessionmaker(bind=engine)
-  session = Session()
-  query_string = "SELECT CallingStationId, CalledStationId FROM {0} WHERE UserName = :username ORDER BY AcctStartTime DESC LIMIT 1;".format(mysqldb_config.radius_acct_table)
-  if (query_result := session.execute(query_string, {'username': username}).first()):
-    mac = query_result['CallingStationId']
-    vlan_name = query_result['CalledStationId']
-    pon = format_pon_name(vlan_name)
-    if (onu_id := get_onu_id_by_mac(mac, pon)):
-      return onu_id
-    if vlan_name[3:5] != '00':
-      return 'Usuário está desconectado. Última conexão através da ONU da {0}.\nPossíveis problemas:\n- roteador desligado, travado ou desconectado da ONU;\n- ONU travada, sem sinal ou desligada. Verifique o sinal da ONU com o comando "/sinal {1}".'.format(sanitize_cto_vlan_name(vlan_name), vlan_name[1:5])
-    if pon:
-      board_number = '12' if vlan_name[1:2] == '1' else '14'
-      return 'Usuário está desconectado. Última conexão através de FIBRA na Placa {0} PON {1}.\nPossíveis problemas:\n- roteador desligado ou desconectado da ONU;\n- ONU travada, sem sinal ou desligada.'.format(board_number, vlan_name[2:3])
-    return 'Usuário não conecta por ONU - vlan: {0}'.format(vlan_name[1:5])
-  return None
+  onu_id = cto_name = diagnostic = ''
+  session = get_mysql_session()
+  query_acct = "SELECT CallingStationId, CalledStationId FROM {0} WHERE UserName = :username ORDER BY AcctStartTime DESC LIMIT 1;".format(mysqldb_config.radius_acct_table)
+  query_postauth = "SELECT sucess, pass, CallingStationId, CalledStationId FROM {0} WHERE user = :username ORDER BY date DESC LIMIT 1;".format(mysqldb_config.radius_postauth_table)
+  if (query_result := session.execute(query_acct, {'username': username}).first()):
+    pon = format_pon_name(query_result['CalledStationId'])
+    cto_name = sanitize_cto_vlan_name(query_result['CalledStationId'])
+    if not (onu_id := get_onu_id_by_mac(query_result['CallingStationId'], pon)):
+      diagnostic = diagnose_onu_not_found(pon, query_result, cto_name)
+  elif (query_result := session.execute(query_postauth, {'username': username}).first()):
+    pon = format_pon_name(query_result['CalledStationId'])
+    cto_name = sanitize_cto_vlan_name(query_result['CalledStationId'])
+    if (onu_id := get_onu_id_by_mac(query_result['CallingStationId'], pon)):
+      diagnostic = diagnose_login(session, query_result, username)
+    else:
+      diagnostic = diagnose_onu_not_found(pon, query_result, cto_name)
+  session.close()
+  return {'onu_id': onu_id, 'cto_name': cto_name, 'diagnostic': diagnostic}
 
 def main():
   parser = argparse.ArgumentParser()

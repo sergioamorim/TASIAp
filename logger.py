a5086982097381a800b80e31f24c2343c312561a
sergio <sergioatamorim@gmail.com>
2020-03-11 08:59:28 -0300

    better handling of external modules names

--- logger.py
+++ logger.py
@@ -1,14 +1,28 @@
-import logging
+#!/usr/bin/env python3.8
+# coding=utf-8
+
+from inspect import stack
+from logging import getLogger, DEBUG, Formatter, FileHandler, StreamHandler
+from pathlib import Path
+from re import findall
 
 from common.string_common import get_caller_name
 from config import bot_config
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-file_handler = logging.FileHandler(bot_config.logs_path + __name__)
+
+if result := findall('.*/(.*?).py', stack()[-1].filename):
+  module_name = result[0]
+else:
+  module_name = stack()[-1].filename[:-3]
+
+logger = getLogger(module_name)
+logger.setLevel(DEBUG)
+formatter = Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_path = Path(bot_config.logs_path + module_name + '.log')
+Path(bot_config.logs_path).mkdir(parents=True, exist_ok=True)
+file_handler = FileHandler(file_path)
 file_handler.setFormatter(formatter)
-stream_handler = logging.StreamHandler()
+stream_handler = StreamHandler()
 stream_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 logger.addHandler(stream_handler)
@@ -30,10 +44,10 @@ def get_last_name(update, query=False):
 
 
 def get_username(update, query=False):
-  if query:
-    return ' - @{0} '.format(update.callback_query.message.chat.username) if update.callback_query.message.chat.username \
+  if not query:
+    return ' - @{0} '.format(update.message.from_user.username) if update.message.from_user.username else ''
+  return ' - @{0} '.format(update.callback_query.message.chat.username) if update.callback_query.message.chat.username \
       else ''
-  return ' - @{0} '.format(update.message.from_user.username) if update.message.from_user.username else ''
 
 
 def log_update(update, logger):

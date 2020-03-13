a179f60de3eaade30bae0028fe03028e62ad88c7
sergio <sergioatamorim@gmail.com>
2020-03-13 10:47:00 -0300

    better handling of diagnose fail

--- find_next_onu_connection.py
+++ find_next_onu_connection.py
@@ -16,13 +16,14 @@ def one_day_has_passed(start_time, actual_time):
 
 
 def diagnose_fail(session, user):
-  if actual_pass := session.execute('SELECT pass FROM {0} WHERE user = :username;'.format(mysqldb_config.login_table),
-                                    {'username': user['user']}).scalar():
-    if actual_pass == user['pass']:
+  if result := session.execute('SELECT user, pass FROM {0} WHERE user = :username;'.format(mysqldb_config.login_table),
+                               {'username': user['user']}).first():
+    if result['pass'] == user['pass']:
       return diagnose_account(session, user)
-    return 'erro, senha do usuário errada.\nSenha recebida: {0}\nSenha correta: {1}'.format(user['pass'], actual_pass)
-  if session.execute({'username': user['user']}).first():
-    return 'erro, usuário sem senha de login cadastrada no sistema.'
+    if result['pass'] == 'None':
+      return 'erro, usuário sem senha de login cadastrada no sistema.'
+    return 'erro, senha do usuário errada.\nSenha recebida: {0}\nSenha correta: {1}'.format(user['pass'],
+                                                                                            result['pass'])
   return 'erro, não existe login com esse usuário.'
 
 

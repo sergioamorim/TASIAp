7862af387cac0471dac8c73a2b6907860c20cf0b
sergio <sergioatamorim@gmail.com>
2020-03-14 10:25:59 -0300

    fixed method usage that was raising AttributeError

--- cto_by_name.py
+++ cto_by_name.py
@@ -11,9 +11,10 @@ def find_cto_by_name(string_list):
   session = get_mysql_session()
   sql_query_string = "SELECT DISTINCT CalledStationID FROM {0} WHERE CalledStationID LIKE '%{1}%' ORDER BY " \
                      "AcctStartTime DESC LIMIT 1;".format(mysqldb_config.radius_acct_table, '%'.join(string_list))
-  query_results = session.execute(sql_query_string).findall()
+  query_results = session.execute(sql_query_string)
   ctos_found = []
   for query_result in query_results:
     ctos_found.append(sanitize_cto_vlan_name(query_result['CalledStationID']))
+  session.close()
   logger.info('find_cto_by_name({0}): {1}'.format(string_list, ctos_found))
   return ctos_found

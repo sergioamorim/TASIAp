d6ab7e68a47f5133cd707ac421d7c3d05f41227f
sergio <sergioatamorim@gmail.com>
2020-03-14 10:16:35 -0300

    added finding cto by name functionality to /procurar

---
+++ cto_by_name.py
@@ -0,0 +1,18 @@
+from common.mysql_common import get_mysql_session
+from common.string_common import sanitize_cto_vlan_name
+from config import mysqldb_config
+from logger import get_logger
+
+logger = get_logger(__name__)
+
+
+def find_cto_by_name(string_list):
+  logger.debug('find_cto_by_name: {0}'.format(string_list))
+  session = get_mysql_session()
+  sql_query_string = "SELECT DISTINCT CalledStationID FROM {0} WHERE CalledStationID LIKE '%{1}%' ORDER BY " \
+                     "AcctStartTime DESC LIMIT 1;".format(mysqldb_config.radius_acct_table, '%'.join(string_list))
+  query_results = session.execute(sql_query_string).all()
+  ctos_found = []
+  for query_result in query_results:
+    ctos_found.append(sanitize_cto_vlan_name(query_result['CalledStationID']))
+  return ctos_found
--- handlers/procurar.py
+++ handlers/procurar.py
@@ -2,6 +2,7 @@ from telegram import ParseMode
 
 from common.bot_common import is_user_authorized
 from common.string_common import format_clients_message
+from cto_by_name import find_cto_by_name
 from logger import log_update, get_logger
 from username_by_name import find_username_by_name
 
@@ -11,14 +12,21 @@ logger = get_logger(__name__)
 def procurar(update, context):
   log_update(update, logger)
   if is_user_authorized(update.message.from_user.id):
-    if not len(context.args):
+    if not (args_len := len(context.args)):
       update.message.reply_text(
         'Envie "/procurar maria" para receber a lista de clientes que contenham maria no nome, endereço, '
-        'login ou observações.',
+        'login ou observações. Envie "/procurar cto leisiane" para procurar CTOs com leisiane no nome.',
         quote=True)
     else:
-      name = ' '.join(context.args)
-      update.message.reply_text(format_clients_message(name, find_username_by_name(name)), parse_mode=ParseMode.HTML,
-                                quote=True)
+      if context.args[0] == 'cto':
+        if args_len > 1:
+          update.message.reply_text('{0}'.format('/n'.join(find_cto_by_name(context.args[1:]))), quote=True)
+        else:
+          update.message.reply_text('Envie "/procurar cto leisiane" para procurar CTOs com leisiane no nome.',
+                                    quote=True)
+      else:
+        name = ' '.join(context.args)
+        update.message.reply_text(format_clients_message(name, find_username_by_name(name)), parse_mode=ParseMode.HTML,
+                                  quote=True)
   else:
     update.message.reply_text('Você não tem permissão para acessar o handlers /procurar.', quote=True)

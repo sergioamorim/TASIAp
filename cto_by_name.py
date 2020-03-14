b7d3f4103f262bde1472fb5e5f018f2ded50bed7
sergio <sergioatamorim@gmail.com>
2020-03-14 10:21:32 -0300

    bug fix - ResultProxy method was misspelled

--- cto_by_name.py
+++ cto_by_name.py
@@ -7,12 +7,13 @@ logger = get_logger(__name__)
 
 
 def find_cto_by_name(string_list):
-  logger.debug('find_cto_by_name: {0}'.format(string_list))
+  logger.debug('find_cto_by_name({0})'.format(string_list))
   session = get_mysql_session()
   sql_query_string = "SELECT DISTINCT CalledStationID FROM {0} WHERE CalledStationID LIKE '%{1}%' ORDER BY " \
                      "AcctStartTime DESC LIMIT 1;".format(mysqldb_config.radius_acct_table, '%'.join(string_list))
-  query_results = session.execute(sql_query_string).all()
+  query_results = session.execute(sql_query_string).findall()
   ctos_found = []
   for query_result in query_results:
     ctos_found.append(sanitize_cto_vlan_name(query_result['CalledStationID']))
+  logger.info('find_cto_by_name({0}): {1}'.format(string_list, ctos_found))
   return ctos_found

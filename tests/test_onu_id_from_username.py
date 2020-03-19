463c4b3d4a398c76ecf952b0f561b9f371efde90
sergio <sergioatamorim@gmail.com>
2020-03-19 19:49:57 -0300

    setup unit tests on onu_id_from_username and onu_signal_power - get_onu_number function from onu_id_from_username module is now being unit tested; - get_signal_power function from onu_signal_power module is now being unit tested.

---
+++ tests/test_onu_id_from_username.py
@@ -0,0 +1,21 @@
+import unittest
+
+from onu_id_from_username import get_onu_number
+
+
+class TestOutputMethods(unittest.TestCase):
+
+  def test_get_onu_number(self):
+
+    success = 'show pon_mac slot 12 link 1 lookup E4BEED22E4F3\r\n-----  PON MAC ADDRESS, ITEM=208 ' \
+              '-----\n\r203\tE4:BE:ED:22:E4:F3\t Vid:1119\t OnuId:19\n\rAdmin\\gponline# '.encode('ascii').decode(
+                                                                                                                'ascii')
+    error = 'show pon_mac slot 12 link 1 lookup A1AAAA11A1A1\r\n-----  PON MAC ADDRESS, ITEM=208 ' \
+            '-----\n\rAdmin\\gponline# '.encode('ascii').decode('ascii')
+
+    self.assertEqual(get_onu_number(success), '19')
+    self.assertEqual(get_onu_number(error), None)
+
+
+if __name__ == '__main__':
+    unittest.main()

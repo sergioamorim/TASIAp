54665c5eb50dc5b71396467d524fbd67b0a8f70d
sergio <sergioatamorim@gmail.com>
2020-03-20 11:48:58 -0300

    improvements on test cases and code refactoring - removed function get_next_value from telnet_common module since it is not being used anymore; - binary strings are now better simulated on tests for onu_id_from_username and onu_signal_power modules; - user_from_onu module code refactored to use regex instead of Telnet's read_until; - test case for get_mac_list function on user_from_onu module deployed; - removed debug_user module.

--- tests/test_onu_signal_power.py
+++ tests/test_onu_signal_power.py
@@ -7,19 +7,19 @@ class TestOutputMethods(unittest.TestCase):
 
   def test_get_signal_power(self):
 
-    success = "show optic_module slot 12 link 1 onu 1\r\n-----  ONU OPTIC MODULE PAR INFO 10.1.1-----\r\nNAME    " \
-                   "      VALUE\tUNIT\r\n---------------------------------------\r\nTYPE         : 20\t(" \
-                   "KM)\r\nTEMPERATURE  : 47.00\t('C)\r\nVOLTAGE      :  3.25\t(V)\r\nBIAS CURRENT : 18.78\t(" \
-                   "mA)\r\nSEND POWER   :  2.82\t(Dbm)\r\nRECV POWER   : -25.08\t(Dbm)\r\nOLT RECV POWER :  0.00\t(" \
-                   "Dbm)\r\nAdmin\\gpononu# ".encode('ascii').decode('ascii')
-    error_signal = 'show optic_module slot 14 link 1 onu 7\r\n[ ERR -553 ] onu is in ' \
-                   'unactive!\n\rAdmin\\gpononu# '.encode('ascii').decode('ascii')
-    error_noauth = 'show optic_module slot 14 link 1 onu 99\r\n[ ERR -506 ] the onu is not ' \
-                   'authorized!\n\rAdmin\\gpononu# '.encode('ascii').decode('ascii')
-    error_unknown = 'show optic_module slot 14 link 1 onu 99\r\n[ ERR 85 ] simulated error ' \
-                    '\n\rAdmin\\gpononu# '.encode('ascii').decode('ascii')
-    fail_unknown = 'show optic_module slot 14 link 1 onu 99\r\nsimulated unknown ' \
-                   'output\n\rAdmin\\gpononu# '.encode('ascii').decode('ascii')
+    success = b"show optic_module slot 12 link 1 onu 1\r\n-----  ONU OPTIC MODULE PAR INFO 10.1.1-----\r\nNAME    " \
+              b"      VALUE\tUNIT\r\n---------------------------------------\r\nTYPE         : 20\t(" \
+              b"KM)\r\nTEMPERATURE  : 47.00\t('C)\r\nVOLTAGE      :  3.25\t(V)\r\nBIAS CURRENT : 18.78\t(" \
+              b"mA)\r\nSEND POWER   :  2.82\t(Dbm)\r\nRECV POWER   : -25.08\t(Dbm)\r\nOLT RECV POWER :  0.00\t(" \
+              b"Dbm)\r\nAdmin\\gpononu# ".decode('ascii')
+    error_signal = b'show optic_module slot 14 link 1 onu 7\r\n[ ERR -553 ] onu is in ' \
+                   b'unactive!\n\rAdmin\\gpononu# '.decode('ascii')
+    error_noauth = b'show optic_module slot 14 link 1 onu 99\r\n[ ERR -506 ] the onu is not ' \
+                   b'authorized!\n\rAdmin\\gpononu# '.decode('ascii')
+    error_unknown = b'show optic_module slot 14 link 1 onu 99\r\n[ ERR 85 ] simulated error ' \
+                    b'\n\rAdmin\\gpononu# '.decode('ascii')
+    fail_unknown = b'show optic_module slot 14 link 1 onu 99\r\nsimulated unknown ' \
+                   b'output\n\rAdmin\\gpononu# '.decode('ascii')
 
     self.assertEqual(get_signal_power(success), '-25.08')
     self.assertEqual(get_signal_power(error_signal), 'off')

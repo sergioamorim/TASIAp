f58667cd32523f327bfae2769883dc25f954dd59
sergio <sergioatamorim@gmail.com>
2020-06-25 23:35:23 -0300

    changed the unit test for snmpset_hex from snmp.common - changed the unit test for the function snmpset_hex from snmp.common module to test more accurately the command execution - the subprocess run is now patched and the execution doesn't actually happens, but the arguments used in the patched function are asserted.

--- tests/test_snmp/test_common.py
+++ tests/test_snmp/test_common.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from unittest.mock import patch, call
 
 from config import snmp_config
 from tasiap.snmp.common import snmpset_hex
@@ -6,18 +7,20 @@ from tasiap.snmp.common import snmpset_hex
 
 class TestFunctions(TestCase):
 
-  def test_snmpset_hex(self):
+  @patch('tasiap.snmp.common.run')
+  def test_snmpset_hex(self, mocking_run):
     test_a_snmp_oid = 'a'
     test_a_hex_string = 'b'
 
-    test_a_result = '' \
+    test_a_command = '' \
       '{snmpset_bin} -v 2c -c {snmp_community} {snmp_ip}:{snmp_port} {snmp_oid} x \'{hex_string}\'' \
       ''.format(
         snmpset_bin=snmp_config.snmpset_bin_path, snmp_community=snmp_config.community,
         snmp_ip=snmp_config.ip, snmp_port=snmp_config.port, snmp_oid=test_a_snmp_oid, hex_string=test_a_hex_string
       )
 
-    self.assertEqual(
-      first=test_a_result,
-      second=snmpset_hex(snmp_oid=test_a_snmp_oid, hex_string=test_a_hex_string)
-    )
+    tast_a_calls = [call(test_a_command, shell=True)]
+
+    snmpset_hex(snmp_oid=test_a_snmp_oid, hex_string=test_a_hex_string)
+
+    self.assertEqual(first=tast_a_calls, second=mocking_run.call_args_list)
